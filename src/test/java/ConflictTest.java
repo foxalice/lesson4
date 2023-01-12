2ede6e45d3264a93038086f42c8a8f95e2232012
Saveleva Natalia <n.saveleva@itssupport.ru>
2023-01-12 16:54:42 +0400

    First test add

---
+++ src/test/java/ConflictTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ConflictTest {
+    @Test
+    void JustTestForLesson() {
+
+        open("https://github.com");
+
+        $(".header-menu-wrapper").$(byText("Solutions")).hover();
+        $(withText("Enterprise")).click();
+        $(".h1-mktg").shouldHave(text("Build like the best"));
+
+        sleep(5000);
+
+    }
+}
